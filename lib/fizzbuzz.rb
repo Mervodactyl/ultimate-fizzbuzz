92f1d249b3d07982ee5a9738b956435aed6664a1
Mervodactyl <merve.silk@gmaillcom>
2014-08-14 11:52:35 +0100

    passed Fizz test

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,19 @@
 def is_divisible_by_three?(number)
-  number % 3 == 0
+  is_divisible_by?(3, number)
 end
 
 def is_divisible_by_five?(number)
-  number % 5 == 0
+  is_divisible_by?(5, number)
+end
+
+def is_divisible_by_fifteen?(number)
+  is_divisible_by?(15, number)
+end
+
+def is_divisible_by?(divisor, number)
+  number % divisor == 0
+end
+
+def fizzBuzz(number)
+  return "Fizz"
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,6 @@
-require 'fizzbuzz'
+require 'fizzBuzz'
 
-describe 'fizzbuzz' do
+describe 'fizzBuzz' do
 	it 'should know 3 is divisible by 3' do
 	expect(is_divisible_by_three?(3)).to be true
 	end
@@ -16,5 +16,18 @@ describe 'fizzbuzz' do
   it 'should know 1 is not divisible by 5' do
     expect(is_divisible_by_five?(1)).to be false
   end
+
+  it 'should know 15 is divisible by 15' do
+    expect(is_divisible_by_fifteen?(15)).to be true
+  end
+
+  it 'should know 1 is not divisible by 15' do
+    expect(is_divisible_by_fifteen?(1)).to be false
+  end
+
+  it 'should return Fizz when the number is divisible by 3' do
+    expect(fizzBuzz(3)).to eq "Fizz"
+  end
+
 
 end

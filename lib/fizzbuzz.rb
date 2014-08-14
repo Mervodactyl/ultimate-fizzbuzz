2159904cc047b902e40834e5a4b83c30686b871c
Mervodactyl <merve.silk@gmaillcom>
2014-08-14 11:00:10 +0100

    Fixed failing divides by 3 test, added failing divides by 5 test

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,5 @@
+
+
+  def is_divisible_by_three?(number)
+    true
+  end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,11 @@
 require 'fizzbuzz'
 
-expect 'fizzbuzz' do
+describe 'fizzbuzz' do
 	it 'should know 3 is divisible by 3' do
 	expect(is_divisible_by_three?(3)).to be true
 	end
+
+  it 'should know 5 is divisible y 5' do
+    expect(is_divisible_by_five?(5)).to be true
+  end
 end
